1194109bcc224d7e2d9836ce672f209e3cb46061
BAlshowaikh <batoolalshowaikh04@outlook.com>
2025-11-13 11:07:07 +0300

    Modified partial views

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(morgan("dev"))
 
+// This means: Everything inside the /public folder is allowed to be served to the browser as static files not a route.
+app.use('/public', express.static('public'))
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -30,6 +33,13 @@ app.use(
 //Load AuthRoute
 const authRouter = require("./src/routes/auth")
 app.use("/auth", authRouter)
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+})
+
 //Listen to port
-app.listen(port)
+app.listen(port, () => {
+  console.log(`App is listening in port ${process.env.PORT}`)
+})
 
